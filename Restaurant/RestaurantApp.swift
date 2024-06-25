18e0a26dab17dc6416809419a3768a7b93be7fd4
Tauan Camargo <lukyhenson@gmail.com>
2024-06-25 01:57:57 -0300

    Initial Commit

---
+++ Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Tauan Tathiell Camargo on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
