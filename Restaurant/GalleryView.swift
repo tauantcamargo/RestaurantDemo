bbdffde67dc3f63ec9e845a938661b9bdf56b61f
Tauan Camargo <lukyhenson@gmail.com>
2024-06-25 20:33:25 -0300

    feat: create gallery using geometry reader

--- Restaurant/GalleryView.swift
+++ Restaurant/GalleryView.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct GalleryView: View {
+    var dataService = DataService()
+    @State var photos: [String] = [String]()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text("Gallery")
+                .font(.largeTitle)
+                .bold()
+            
+            GeometryReader {proxy in
+                ScrollView(showsIndicators: false) {
+                    LazyVGrid(columns: [GridItem(spacing: 10),
+                                        GridItem(spacing: 10),
+                                        GridItem(spacing: 10)], spacing: 10) {
+                        ForEach(photos, id: \.self) { photo in
+                            Image(photo)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(maxWidth: (proxy.size.width - 20) / 3)
+                                .clipped()
+                        }
+                    }
+                    .onAppear() {
+                        photos = dataService.getPhotos()
+                    }
+                }
+            }
+        }
+        .padding()
     }
 }
 
