02a344a142ebe7f3c4d19521456813d944604cce
Tauan Camargo <lukyhenson@gmail.com>
2024-06-25 08:46:12 -0300

    feat: add about content

--- Restaurant/AboutView.swift
+++ Restaurant/AboutView.swift
@@ -9,10 +9,31 @@ import SwiftUI
 
 struct AboutView: View {
     var body: some View {
-        VStack {
-            Text("About")
-                .font(.largeTitle)
-                .bold()
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .leading) {
+                Text("About")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .bold()
+                
+                Image("restaurant")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.bottom)
+                
+                Text("Excepteur tempor nostrud ut incididunt velit ad Lorem. Mollit occaecat Lorem ea. Reprehenderit velit sint ullamco minim. Elit officia reprehenderit eu nulla ex do minim qui. Consequat anim ex irure aliquip eu adipisicing aliquip consequat dolore non enim adipisicing. Aliquip est amet culpa magna aute labore enim cillum consequat labore duis occaecat laborum. Commodo amet fugiat nostrud in mollit nisi est est ex et do proident consequat nostrud.")
+                
+                Image("map")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                Text("""
+                    Avenida Carlos Elias
+                    Quadra 17 Lote 19
+                    Sao Carlos
+                    """)
+                
+            }
+            .padding(.horizontal)
         }
     }
 }
